d54beea3486ee51e467765c6e8876bace3d995db
Isabel Velazquez <isabelv4807@hstat.org>
2021-03-07 05:00:52 +0000

    quick initial code ideas

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://github.com/hstatsep-students/jsproject2-isabelv4807-oltionap0876-adonesek6862.git',
     dataType: 'json',
     success: function(data) {
         console.log(data);
     }
 });
+/*
+alert("Hello, we've created a tool that gives you the name of the schools with a certain SAT score");
+var satSection = prompt("What section of the SAT would you like to look at? (critical reading, writing or math)");
+alert("Great choice! Now, what scores are you looking for the school to have?");
+if (satSection = critical reading)// Reminder for isabel: remember to code the right way this is just a "notecatcher "
+var scoreRangeCriticalReading = promt("Enter the score between 200-800 that you want to see and we'll give you the schools with that score");
+if (satSection = writing)
+if (satSection = math)
+// Reminder for isabel: remember to code the right way this is just a "notecatcher "
+*?
