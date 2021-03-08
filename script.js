b8a7e09dfa3af78b1fd913510f1526d9fee3dd29
Isabel Velazquez <isabelv4807@hstat.org>
2021-03-08 15:52:35 +0000

    functions

--- script.js
+++ script.js
@@ -6,28 +6,30 @@ $.ajax({
     }
 });
 
-alert("Hello, we've created a tool that gives you the name of the schools with a certain SAT score");
+alert("Hello, we've created a tool that gives you the name of the schools with a certain SAT score"); //Introduction
 
-var satSection = prompt("What section of the SAT would you like to look at? (critical reading, writing or math)");
+var satSection = prompt("What section of the SAT would you like to look at? (critical reading, writing or math)"); // asks for user input 
 var userSection = function(satSection) {
     if ( satSection === critical reading){
+        alert("Great choice! Now, what scores are you looking for the school to have?");
         return 
     }
     if ( satSection === writing){
+        alert("Great choice! Now, what scores are you looking for the school to have?");
         return 
     }
     if ( satSection === math){
+        alert("Great choice! Now, what scores are you looking for the school to have?");
         return 
     }
     
-};
+}; // function will return a question based on the section
 
 
-
-/*alert("Great choice! Now, what scores are you looking for the school to have?");
-if (satSection = critical reading)// Reminder for isabel: remember to code the right way this is just a "notecatcher "
 var scoreRangeCriticalReading = promt("Enter the score between 200-800 that you want to see and we'll give you the schools with that score");
+
+if (satSection = critical reading)// Reminder for isabel: remember to code the right way this is just a "notecatcher "
 if (satSection = writing)
 if (satSection = math)
 // Reminder for isabel: remember to code the right way this is just a "notecatcher "
-*/
+
