6ca4d2282dea320a9e954e2cd23614c963f12cbf
Isabel Velazquez <isabelv4807@hstat.org>
2021-03-10 07:01:22 +0000

    finished conditionals to get user info

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 $.ajax({
-    url: 'https://data.cityofnewyork.us/Education/2012-SAT-Results/f9bf-2cp4',
+    url: 'https://data.cityofnewyork.us/resource/f9bf-2cp4.json',
     dataType: 'json',
     success: function(data) {
         console.log(data);
     }
 });
 
-alert("Hello, we've created a tool that gives you the name of the schools with a certain SAT score"); //Introduction
+alert ("Hello, we've created a tool that gives you the name of the schools with a certain SAT score"); //Introduction
 
-var satSection = prompt("What section of the SAT would you like to look at? (critical reading, writing or math)"); // asks for user input 
-var userSection = function(satSection) {
-    if ( satSection === critical reading){
-        alert("Great choice! Now, what scores are you looking for the school to have?");
-        return 
-    }
-    if ( satSection === writing){
-        alert("Great choice! Now, what scores are you looking for the school to have?");
-        return 
-    }
-    if ( satSection === math){
-        alert("Great choice! Now, what scores are you looking for the school to have?");
-        return 
-    }
-    
-}; // function will return a question based on the section
+var satSection = prompt("What section of the SAT would you like to look at? (critical reading, writing or math)");// asks for user input 
+document.write ("<br>"+ "Section: " + satSection + "<br>"); // writes user input on screen
 
+alert("Great choice! Now, what scores are you looking for the school to have?"); 
 
-var scoreRangeCriticalReading = promt("Enter the score between 200-800 that you want to see and we'll give you the schools with that score");
-var 
-
-if (satSection = critical reading)// Reminder for isabel: remember to code the right way this is just a "notecatcher "
-if (satSection = writing)
-if (satSection = math)
-// Reminder for isabel: remember to code the right way this is just a "notecatcher "
-
+if (satSection === "critical reading" , " Critical Reading"){
+    var criticalReadingSatScore = prompt( "Enter a score between 200 and 800 ie: 355");
+    alert(" Generating a list of schools with the SAT score of " + criticalReadingSatScore + " in the " + satSection + " section " );
+    document.write ("<br>"+ "Score: " + criticalReadingSatScore + "<br>"); // writes what score user is looking for
+} else if (satSection === "Math", "math") {
+    var mathSatScore =  prompt( "Enter a score between 200 and 800 ie: 244");
+    alert(" Generating a list of schools with the SAT score of " + mathSatScore + " in the " + satSection + " section " );
+    document.write ("<br>"+ "Score: " + mathSatScore + "<br>");
+} else if (satSection === " Writing", "writing"){
+    var writingSatScore = prompt("Enter a score between 200 and 800 ie: 453");
+    alert(" Generating a list of schools with the SAT score of " + writingSatScore + " in the " + satSection + " section " );
+    document.write ("<br>"+ "Score: " + writingSatScore + "<br>");
+}
+//  creates a conditional for each SAT section then asks for a score they are looking for
