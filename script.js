e31dbcd8b7ce5531c6f703a5a5c5afb4720a7509
Isabel Velazquez <isabelv4807@hstat.org>
2021-03-15 06:06:43 +0000

    final

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 
 // Intro to out project
-alert ("Hello, we've created a tool that gives you the name of the schools with a certain SAT score");  
+alert ("Hello, we've created a tool that gives you the name of the schools with a certain SAT score in the critical reading section");  
 
-// asks for user input on what section they want to look at and puts their choice on the screen
-var satSection = prompt("What section of the SAT would you like to look at? (critical reading, writing or math)");
-document.write ("<br>"+ "Section: " + satSection + "<br>"); // writes user input on screen
-
-
-alert("Great choice! Now, what scores are you looking for the school to have?");
+// asks to answer a question based on the alert if its write they can continue if not they have to try again
+var satSection = prompt(" Wait... what section of the SAT we're supposed to like to look at? (hint:critical reading)");
+document.write ("<br>"+ "Section: " + satSection+ "<br>"); // writes user input on screen
 
 
-//  creates a conditional for each SAT section then asks for a score they are looking for based on the section they want 
-if (satSection === "critical reading" , " Critical Reading"){
-    var criticalReadingSatScore = prompt( "Enter a score between 200 and 800 ie: 355");
+
+//  creates a conditional so if they get the question right they get asked to input the score
+if (satSection === "critical reading"){
+    alert("Ahhh! Thank you for reminding me. Now, what scores are you looking for the school to have?");
+    var criticalReadingSatScore = prompt( "Enter a score between 200 and 800 ie: 355. If nothing shows up then we didn't find a match");
     alert(" Generating a list of schools with the SAT score of " + criticalReadingSatScore + " in the " + satSection + " section " );
     document.write ("<br>"+ "Score: " + criticalReadingSatScore + "<br>");
-    console.log("<br>"+ "Score: " + criticalReadingSatScore + "<br>");// writes what score user is looking for
-} else if (satSection === "Math", "math") {
-    var mathSatScore =  prompt( "Enter a score between 200 and 800 ie: 244");
-    alert(" Generating a list of schools with the SAT score of " + mathSatScore + " in the " + satSection + " section " );
-    document.write ("<br>"+ "Score: " + mathSatScore + "<br>");
-    console.log ("<br>"+ "Score: " + mathSatScore + "<br>");
-} else if (satSection === " Writing", "writing"){
-    var writingSatScore = prompt("Enter a score between 200 and 800 ie: 453");
-    alert(" Generating a list of schools with the SAT score of " + writingSatScore + " in the " + satSection + " section " );
-    document.write ("<br>"+ "Score: " + writingSatScore + "<br>");
-     console.log ("<br>"+ "Score: " + writingSatScore + "<br>");
+    console.log("<br>"+ "Score: " + criticalReadingSatScore + "<br>");
+} else if(satSection != "critical reading" , " Critical Reading"){
+    alert("Nope refresh to try again");
 }
-
 
 $.ajax({
                 url: 'https://data.cityofnewyork.us/resource/f9bf-2cp4.json',
@@ -35,36 +25,16 @@ $.ajax({
                 success: function(data) {
     
                     console.log(data);
-                     for(var i = 0; i < data.length; i++){
-                    
-   /*if( criticalReadingSatScore == data[i].sat_critical_reading_avg_score) {
+  // Loop that ilters through the data and if there are schools with that score it will show you a list but if not nothing will show up on the list                    
+for(var i = 0; i < data.length; i++){
+if( criticalReadingSatScore === data[i].sat_critical_reading_avg_score) {
         document.write("<br>" + " School Name: " + data[i].school_name + "<br>");
         document.write ("<br>"+ "Critical Reading score: " + data[i].sat_critical_reading_avg_score + "<br>");
         document.write ("<hr>");
         console.log("<br>"+ "Score: " + data[i].sat_critical_reading_avg_score + "<br>");
         console.log("<br>" + " School Name: " + data[i].school_name + "<br>");
- 
-   } */
-   if ( mathSatScore == data[i].sat_math_avg_score) {
-                document.write("<br>" + " School Name: " + data[i].school_name + "<br>");
-                document.write("<br>"+ " Math score: " + data[i].sat_math_avg_score + "<br>");
-                document.write("<hr>");
-                console.log("<br>" + " School Name: " + data[i].school_name + "<br>");
-                console.log("<br>"+ "Math score: " + data[i].sat_math_avg_score + "<br>");
-                
-            } 
-    /*if (writingSatScore == data[i].sat_writing_avg_score)
-                document.write("<br>" + " School Name: " + data[i].school_name + "<br>");
-                document.write ("<br>"+ " Writing score: " + data[i].sat_writing_avg_score + "<br>");
-                document.write ("<hr>");
-                console.log("<br>" + " School Name: " + data[i].school_name + "<br>");
-                console.log("<br>"+ "Writing score: " + data[i].sat_writing_avg_score + "<br>");
-               
-               
-               
-             
-    
-            */
-                }}});
+   }
+
+    }}});
                 
 
